a188dd5b291be5195951314ccfefca082258b737
albertoderonzi-primer <alberto@primer.io>
2022-11-25 17:21:48 +0000

    - added support for drop-in vaulted pm - now drop-in can use the same html with custom css

--- src/static/client.js
+++ src/static/client.js
@@ -1,40 +1,178 @@
 window.addEventListener("load", onLoaded);
+
 async function onLoaded() {
-  console.log('Universal Checkout is loaded!')
 
-  const clientSession = await fetch('/client-session', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-  }).then(data => data.json())
-  console.log("Client Session data:", clientSession );
+  const submitFormButton = document.getElementById("submit-button");
+  const autofillFormButton = document.getElementById("autofill-button");
+  const quantity = document.getElementById("quantity");
+  const size = document.getElementById("size");
 
-  const { clientToken } = clientSession
-  console.log( "Client token:",clientToken );
+  const customerDetails = {
+    firstName: document.getElementById("first-name"),
+    lastName: document.getElementById("last-name"),
+    emailAddress: document.getElementById("email-address"),
+    mobileNumber: document.getElementById("mobile-number"),
+
+  };
+  const billingAddress = {
+    addressLine1: document.getElementById("address-line1"),
+    addressLine2: document.getElementById("address-line2"),
+    city: document.getElementById("city"),
+    state: document.getElementById("state"),
+    postalCode: document.getElementById("postal-code"),
+    country: document.getElementById("country"),
+  };
+
+  const autofillForm = () => {
+    quantity.value = 2;
+    size.value = "l";
+    customerDetails.firstName.value = "Alberto";
+    customerDetails.lastName.value = "DeRonzi";
+    customerDetails.emailAddress.value = "test@primer.io";
+    customerDetails.mobileNumber.value = "+447532172666";
+    billingAddress.addressLine1.value = "1 King Street";
+    billingAddress.addressLine2.value = "2 Floor";
+    billingAddress.city.value = "London";
+    billingAddress.state.value = "GB";
+    billingAddress.postalCode.value = "SE10";
+    billingAddress.country.value = "GB";
+  };
+
+  autofillFormButton.addEventListener("click", autofillForm);
+
+  const clearCheckoutDiv = () => {
+    document.querySelector("#shirt-purchase-form").remove();
+    document
+      .querySelector("#order-container")
+      .setAttribute("id", "checkout-container");
+  };
 
 
-  const universalCheckout = await Primer.showUniversalCheckout(clientToken, {
-    // Specify the selector of the container element
-    container: '#checkout-container',
+  const getOrderInfo = (currency) => {
+    return {
+      customerId: "cust-1229",
+      orderId: `${Math.random().toString(36).substring(7)}`,
+      currencyCode: currency || "GBP",
+      order: {
+        lineItems: [
+          {
+            itemId: `item-${size.value}`,
+            name: `${quantity.value} Lego${quantity.value > 1 ? "s" : ""
+              } - ${size.value.toUpperCase()}`,
+            description: `${quantity.value} ${size.value.toUpperCase()} Lego`,
+            amount: 10000 * quantity.value,
+            productType: "PHYSICAL",
+          },
+        ],
+        countryCode: billingAddress.country.value,
+      },
+      customer: {
+        firstName: customerDetails.firstName.value,
+        lastName: customerDetails.lastName.value,
+        emailAddress: customerDetails.emailAddress.value,
+        mobileNumber: customerDetails.mobileNumber.value,
+        billingAddress: {
+          addressLine1: billingAddress.addressLine1.value,
+          addressLine2: billingAddress.addressLine2.value,
+          city: billingAddress.city.value,
+          state: billingAddress.state.value,
+          postalCode: billingAddress.postalCode.value,
+          countryCode: billingAddress.country.value,
+        },
+      },
+      metadata: {
+        env: "headless",
+        Test: "False",
+      },
+    };
+  };
 
-    /**
-     * When the checkout flow has been completed, you'll receive
-     * the successful payment via `onCheckoutComplete`.
-     * Implement this callback to redirect the user to an order confirmation page and fulfill the order.
-     */
-    onCheckoutComplete({ payment }) {
-      console.log('Checkout Complete!', payment)
-    },
 
-    /**
-     * Learn more about the other options at:
-     * https://primer.io/docs
-     * https://www.npmjs.com/package/@primer-io/checkout-web
-     */
+  submitFormButton.addEventListener("click", async () => {
+    const formValid = validateForm();
+    if (formValid === false) {
+      return;
+    }
+    //const clientSession = await getClientSession();
+
+    const orderInfo = getOrderInfo();
+    const clientSession = await fetch('/client-session', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        orderInfo,
+      }),
+    }).then(data => data.json())
+    console.log("Client Session data:", clientSession);
+
+    const { clientToken } = clientSession
+    console.log("Client token:", clientToken);
+
+
+    if (!clientSession) {
+      console.log("Error, no client session");
+      return;
+    } else {
+      console.log("client session: ", clientSession);
+    }
+
+    clearCheckoutDiv();
+
+    await renderCheckout(clientToken);
+  });
+
+
+
+
+
+  const validateForm = () => {
+    if (quantity.value < 1) {
+      alert("Please enter a quantity of at least 1");
+      return false;
+    }
+    for (const value in customerDetails) {
+      if (customerDetails[value].value === "") {
+        alert("Please fill out all customer details");
+        return false;
+      }
+    }
+    for (const value in billingAddress) {
+      if (billingAddress[value].value === "") {
+        alert("Please fill out all billing address fields");
+        return false;
+      }
+    }
+  };
+
+
+  const renderCheckout = async (clientToken) => {
+
+    const universalCheckout = await Primer.showUniversalCheckout(clientToken, {
+      // Specify the selector of the container element
+      container: '#checkout-container',
+
+      /**
+       * When the checkout flow has been completed, you'll receive
+       * the successful payment via `onCheckoutComplete`.
+       * Implement this callback to redirect the user to an order confirmation page and fulfill the order.
+       */
+      onCheckoutComplete({ payment }) {
+        console.log('Checkout Complete!', payment)
+      },
+
+      /**
+       * Learn more about the other options at:
+       * https://primer.io/docs
+       * https://www.npmjs.com/package/@primer-io/checkout-web
+       */
+    }
+
+
+    )
+
+
+
   }
-
-
-)
-
-
-
 }
